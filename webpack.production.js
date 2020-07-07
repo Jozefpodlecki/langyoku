e77b0cff5367a0354b3534a01202f9102b7c6187
Jozef Podlecki <jozef.witold.podlecki@gmail.com>
2020-07-07 11:56:23 +0100

    deploy

--- webpack.production.js
+++ webpack.production.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     output: {
         path: __dirname,
-        publicPath: '/',
+        publicPath: '/langyoku/',
         filename: '[name].bundle.js'
     },
     resolve: {
